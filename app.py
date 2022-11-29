74d1997ff886b4bcadc6b611175826b300fd8931
Evanderss <evanderramirez150@gmail.com>
2022-11-28 23:30:26 -0600

    Creando formulario e importando librerias

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional, Text
+from datetime import datetime
+
 
 app = FastAPI()
 
+
 posts = []
+
+
+# Post model
+class Post(BaseModel):
+    id: Optional[str]
+    title: str
+    author: str
+    content: Text
+    created_at: datetime =  datetime.now()
+    published_at: Optional[datetime] 
+    published: Optional[bool] = False
+
 
 @app.get("/")
 def read_root():
     return {"welcome": "Welcome to my REST API"}
 
+
 @app.get("/posts")
 def get_posts():
     return posts
+
 
 @app.post("/posts")
 def save_post(post):
