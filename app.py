924e741858072293ea4d14ce8a2ffed27ba17c02
Evanderss <evanderramirez150@gmail.com>
2022-12-07 12:00:07 -0600

    creando la ruta delete

--- app.py
+++ app.py
@@ -45,3 +45,12 @@ def get_post(post_id: str):
         if post["id"] == post_id:
             return post
     raise HTTPException(status_code=404, detail="Item not found")
+
+
+@app.delete('/posts/{post_id}')
+def delete_post(post_id: str):
+    for index, post in enumerate(posts):
+        if post["id"] == post_id:
+            posts.pop(index)
+            return {"message": "Post has been deleted succesfully"}
+    raise HTTPException(status_code=404, detail="Item not found")
